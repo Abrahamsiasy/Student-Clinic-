c9efe0d7e76315f1ba7a2361ba745523fc40966c
Mikiyas Abadir Degefa <mikiyas.degefa@ju.edu.et>
2023-12-12 14:09:56 +0300

    Failed to load DOM Document 20

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -19,14 +19,14 @@
         </tr>
         <tr>
             <th >
-                &lt;:1 yr</th>
+                &lt;1 yr</th>
             <th>1-4 yrs</th>
             <th>5-14 yrs</th>
             <th>15-29 yrs</th>
             <th>30-64 yrs</th>
             <th>>=65 yrs</th>
             <th>
-                &lt;:1 yr</th>
+                &lt;1 yr</th>
             <th>1-4 yrs</th>
             <th>5-14 yrs</th>
             <th>15-29 yrs</th>
