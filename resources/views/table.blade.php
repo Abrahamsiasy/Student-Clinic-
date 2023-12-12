af898b1401c7733af6b9e64f9b6367de8d2f729d
Mikiyas Abadir Degefa <mikiyas.degefa@ju.edu.et>
2023-12-12 13:57:53 +0300

    Failed to load DOM Document 12

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -86,7 +86,7 @@
 <table>
     <thead>
         <tr>
-            <th colspan="3">Health Facility Monthly OPD Service Report Form</th>
+            <th colspan="3" style="text-align: center">Health Facility Monthly OPD Service Report Form</th>
         </tr>
     </thead>
     <tbody>
