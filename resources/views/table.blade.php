72aa1f5009948ed2ee98e516f35a7cc4ccaa50bf
Mikiyas Abadir Degefa <mikiyas.degefa@ju.edu.et>
2023-12-12 13:51:34 +0300

    Failed to load DOM Document 7

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -82,7 +82,7 @@
     </tbody>
 </table> --}}
 
-<table>
+{{-- <table>
     <tbody>
         <thead>
             <tr>
@@ -93,4 +93,22 @@
 
         </tbody>
     </tbody>
+</table> --}}
+
+<table>
+    <tr>
+      <th>Company</th>
+      <th>Contact</th>
+      <th>Country</th>
+    </tr>
+    <tr>
+      <td>Alfreds Futterkiste</td>
+      <td>Maria Anders</td>
+      <td>Germany</td>
+    </tr>
+    <tr>
+      <td>Centro comercial Moctezuma</td>
+      <td>Francisco Chang</td>
+      <td>Mexico</td>
+    </tr>
   </table>
